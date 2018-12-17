ba8e34bf629152422162e91420bfaa9c9353f958
Reinier Garcia Ramos <reymillenium@gmail.com>
2018-12-17 05:32:47 -0500

    Added validation for root route in order to show or not the banners of the index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,4 @@
 module ApplicationHelper
+
+  # include PagesHelper::show_banners
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
 module PagesHelper
+
+
+  # Determines if the current route is the index or the root
+  # def show_banners
+  #
+  #   if current_page?(:controller => 'pages', :action => 'index') or current_page?(root_url)
+  #   if  current_page?(index_path) ? 'banner' : (current_page?(root_path) ? 'banner' : 'banner-1')
+  #      true
+  #   else
+  #      false
+  #   end
+
+  # end
+
+
+    # <%= current_page?(:controller => 'pages', :action => 'index') ? 'banner' : 'banner-1' %>
 end
