20b3a832e5c7684babc912a78b1bdcb0aaad432a
Reinier Garcia Ramos <reymillenium@gmail.com>
2018-12-25 18:22:07 -0500

    Çhanged again the function show_banners in the pages_helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,14 +5,20 @@ module PagesHelper
   def show_banners
 
     # current_page?(:controller => 'pages', :action => 'index') or current_page?(root_url)
+    #
+    # It works!
     # current_page?(index_path) ? true : (current_page?(root_path) ? true : false)
 
-    if current_page?(index_path) or current_page?(root_path)
-       true
-    else
-       false
-    end
-  end
+    # It works!
+    #   if current_page?(index_path) or current_page?(root_path)
+    #      true
+    #   else
+    #      false
+    #   end
 
+    # It works!
+    (current_page?(index_path) || current_page?(root_path)) ? true : false
+
+  end
 
 end
